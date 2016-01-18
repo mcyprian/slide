054e37f609838f9db10a204c26eee4887268d9b9
Michal Cyprian <m.cyprian@gmail.com>
2016-01-18 20:46:10 +0100

    Add next_run to find problematic add summary of work

--- find_problematic.sh
+++ find_problematic.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-SMT2SL='/home/mcyprian/Codes/smtcomp14-sl/smtlib2sl/compile'
-SLIDE_WRAPPER='/home/mcyprian/Codes/slide/slide/slide-wrapper.sh'
-SLD_DEST='/home/mcyprian/Codes/slide/preds-slrd_entl/'
-SMT_SOURCE_DIR='/home/mcyprian/Codes/smtcomp14-sl/bench/slrd_entl/'
-SLD_UNSUCCESS='/home/mcyprian/Codes/slide/slrd_entl_unsuccess/'
-ERROR_MSG_FILE='/home/mcyprian/Codes/slide/slrd_entl_unsuccess/error_msg'
-ERROR_FREQUENCY_FILE='/home/mcyprian/Codes/slide/slrd_entl_unsuccess/error_num'
+SMT2SL='/home/mcyprian/Codes/devel/smtcomp14-sl/smtlib2sl/compile'
+SLIDE_WRAPPER='/home/mcyprian/Codes/devel/slide/slide/slide-wrapper.sh'
+SLD_DEST='/home/mcyprian/Codes/devel/slide/preds-slrd_entl/'
+SMT_SOURCE_DIR='/home/mcyprian/Codes/devel/smtcomp14-sl/bench/slrd_entl/'
+SLD_UNSUCCESS='/home/mcyprian/Codes/devel/slide/slrd_entl_unsuccess/'
+ERROR_MSG_FILE='/home/mcyprian/Codes/devel/slide/slrd_entl_unsuccess/error_msg_1'
+ERROR_FREQUENCY_FILE='/home/mcyprian/Codes/devel/slide/slrd_entl_unsuccess/error_num_1'
 SLD_SUFFIX='.sld'
 
 if [ $1 == 'clean' ]; then
@@ -16,21 +16,35 @@ if [ $1 == 'clean' ]; then
 fi
 
 if [ $1 == 'generate' ]; then
-for file in $(ls $SMT_SOURCE_DIR); do
-    if [ "$file" == "README" ]; then
-        break
-    fi
-    $SMT2SL -slide $SMT_SOURCE_DIR$file
-    mv $SMT_SOURCE_DIR$file$SLD_SUFFIX $SLD_DEST
-    STDERR=$(($SLIDE_WRAPPER "$SLD_DEST$file$SLD_SUFFIX") 2>&1)
-    retval=$(($?))
-    if [ $retval -ne 0 ]; then
-        cp $SLD_DEST$file$SLD_SUFFIX $SLD_UNSUCCESS$file$SLD_SUFFIX
-        echo "$file$SLD_SUFFIX:" >> "$ERROR_MSG_FILE"
-        echo "$STDERR" >> "$ERROR_MSG_FILE"
-        echo "" >> "$ERROR_MSG_FILE"
-    fi
-done
+    for file in $(ls $SMT_SOURCE_DIR); do
+        if [ "$file" == "README" ]; then
+            break
+        fi
+        $SMT2SL -slide $SMT_SOURCE_DIR$file
+        mv $SMT_SOURCE_DIR$file$SLD_SUFFIX $SLD_DEST
+        STDERR=$(($SLIDE_WRAPPER "$SLD_DEST$file$SLD_SUFFIX") 2>&1)
+        retval=$(($?))
+        if [ $retval -ne 0 ]; then
+            cp $SLD_DEST$file$SLD_SUFFIX $SLD_UNSUCCESS$file$SLD_SUFFIX
+            echo "$file$SLD_SUFFIX:" >> "$ERROR_MSG_FILE"
+            echo "$STDERR" >> "$ERROR_MSG_FILE"
+            echo "" >> "$ERROR_MSG_FILE"
+        fi
+    done
+fi
+
+if [ $1 == 'next_run' ]; then
+    ERROR_MSG_FILE="$ERROR_MSG_FILE"_1 
+    ERROR_FREQUENCY_FILE="$ERROR_FREQUENCY_FILE"_1
+    for file in $(ls $SLD_DEST); do
+        STDERR=$(($SLIDE_WRAPPER "$SLD_DEST$file") 2>&1)
+        retval=$(($?))
+        if [ $retval -ne 0 ]; then
+            echo "$file$SLD_SUFFIX:" >> "$ERROR_MSG_FILE"
+            echo "$STDERR" >> "$ERROR_MSG_FILE"
+            echo "" >> "$ERROR_MSG_FILE"
+        fi
+    done
 fi
 
 echo "Countig frequency of errors:"
@@ -45,6 +59,9 @@ errors=("impossible to join the input"
 "Parsing \%s: now * allowed in empty rules")
 
 cd $SLD_UNSUCCESS
+
+touch $ERROR_MSG_FILE
+touch $ERROR_FREQUENCY_FILE
 
 for ((i = 0; i <  "${#errors[@]}"; i++)); do
     NUM=$((grep "${errors[$i]}" "$ERROR_MSG_FILE" | wc -l) 1>&1)
